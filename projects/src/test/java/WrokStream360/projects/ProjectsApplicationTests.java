c67319e90b5947fcf1d99e6ac230efd33fecc8b6
Farheen khan <farheenkhancc@gmail.com>
2023-05-21 16:49:31 +0530

    Initial commit

---
+++ projects/src/test/java/WrokStream360/projects/ProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package WrokStream360.projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
